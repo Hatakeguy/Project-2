9a323f345a6a45035c8edd3db401838f7b1cabf4
Vedant hatake <166927669+Hatakeguy@users.noreply.github.com>
2025-08-19 15:29:51 +0530

    Update app.py

--- cal-webapp/Backend/app.py
+++ cal-webapp/Backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
@@ -67,4 +68,5 @@ def calc():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render dynamic port
     app.run(host="0.0.0.0", port=port, debug=True)
+
 
